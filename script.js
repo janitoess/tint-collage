6e013c318db7efe4d02b4916f0ba9d718153add2
janitoess <jandmdt@gmail.com>
2026-04-25 00:37:40 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+const upload = document.getElementById("upload");
+const gallery = document.getElementById("gallery");
+
+let baseImage = new Image();
+
+const tintImages = [
+  "tints/red.png",
+  "tints/blue.png",
+  "tints/gold.png"
+];
+
+upload.addEventListener("change", (e) => {
+  baseImage.src = URL.createObjectURL(e.target.files[0]);
+});
+
+function generateCollage() {
+  gallery.innerHTML = "";
+
+  tintImages.forEach((tintSrc) => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const tint = new Image();
+
+    tint.src = tintSrc;
+
+    tint.onload = () => {
+      canvas.width = baseImage.width;
+      canvas.height = baseImage.height;
+
+      ctx.drawImage(baseImage, 0, 0);
+
+      ctx.globalAlpha = 0.6;
+      ctx.drawImage(tint, 0, 0, canvas.width, canvas.height);
+      ctx.globalAlpha = 1;
+
+      const div = document.createElement("div");
+      div.className = "preview";
+      div.appendChild(canvas);
+
+      div.onclick = () => downloadCanvas(canvas);
+
+      gallery.appendChild(div);
+    };
+  });
+}
+
+function downloadCanvas(canvas) {
+  const link = document.createElement("a");
+  link.download = "tint.png";
+  link.href = canvas.toDataURL();
+  link.click();
+}
